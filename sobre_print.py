fa3a62fd3f653c22812086a07834d4ffc0d3d6a4
Arian Weslley <weslleysantoszbd@gmail.com>
2022-11-27 00:38:53 -0300

    Finalização de programa para media de familia

---
+++ sobre_print.py
@@ -0,0 +1,12 @@
+print('PROGRAMA PARA SABER A IDADE MÉDIA DE UMA FAMÍLIA');
+print('_________________________________________________');
+pai = 42;
+mae = 38;
+filho = 18;
+filho2 = 7;
+
+soma = (pai + mae + filho + filho2);
+media = (soma)/4;
+
+
+print('A média da família é:', media); 
